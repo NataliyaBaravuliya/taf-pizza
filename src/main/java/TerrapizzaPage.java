16077e13757bbb07a62e5a676330089100599d06
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-03-16 19:34:24 +0300

    refactor terapizzaPage refactor testPizzaTest

--- src/main/java/TerrapizzaPage.java
+++ src/main/java/TerrapizzaPage.java
@@ -1,14 +1,16 @@
 public class TerrapizzaPage {
     public static final String URL = "https://terrapizza.by/";
     public static final String BTN_MENU = "/html/body/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a";
-    public static final String BTN_PIZZA = "/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[9]/a";
+    public static final String BTN_PIZZA = "/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[10]/a";
     public static final String BTN_CHOOSE_PIZZA = "//*[@id=\"menu-card-364\"]";
     public static final String BTN_CART = "//*[@id=\"modal-product-content\"]/div/div[2]/div[3]/div[2]/div[2]/button";
     public static final String BTN_CLOSE = "//*[@id=\"modal-product-content\"]/button";
     public static final String PRODUCT_PIZZA = "//*[@id=\"basket-el-0\"]/div[2]/div[1]/div[1]";
+
     public static final String BTN_CHECK_CART = "/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]";
-    public static final String BTN_BAR = "/html/body/div[3]/div[1]/div/div/div[1]/div/div/div/ul/li[16]/a";
-    public static final String BTN_RICH = "//*[@id=\"menu-card-1741\"]/div[2]/picture/img";
-    public static final String BTN_CART_RICH = "//*[@id=\"modal-product-content\"]/div/div[2]/div[2]/div[2]/div[2]/button";
-    public static final String PRODUCT_RICH = "//*[@id=\"basket-el-1\"]/div[2]/div[1]/div[1]";
+    public static final String BTN_BAR = "/html/body/div[3]/div[1]/div/div/div[1]/div/div/div/ul[1]/li[17]/a";
+
+   public static final String BTN_RICH = "//*[@id=\"menu-card-1741\"]/div[2]/picture/img";
+   public static final String BTN_CART_RICH = "//*[@id=\"modal-product-content\"]/div/div[2]/div[2]/div[2]/div[2]/button";
+   public static final String PRODUCT_RICH = "//*[@id=\"basket-el-1\"]/div[2]/div[1]/div[1]";
 }
